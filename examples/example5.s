d43e667b0451fbb46dce7fa3a0589bf6ca4a1e4d
Cesar Guedes Carneiro <c261031@dac.unicamp.br>
2024-04-01 17:11:26 -0300

    Fix example 5

--- examples/example5.s
+++ examples/example5.s
@@ -1,11 +1,11 @@
 fld f0, 0(x1)
 fld f1, 0(x2)
 fld f2, 0(x3)
-fmul f0, f0, f0 
-fmul f1, f1, f1 
-fmul f2, f2, f2 
-fadd f3, f0, f1 
-fadd f3, f3, f2 
-fld f4, 0(x0) 
-fdiv f3, f3, f4 
+fmul f0, f0, f0
+fmul f1, f1, f1
+fmul f2, f2, f2
+fadd f3, f0, f1
+fadd f3, f3, f2
+fld f4, 0(x0)
+fdiv f3, f3, f4
 fsd f3, 0(x4)
